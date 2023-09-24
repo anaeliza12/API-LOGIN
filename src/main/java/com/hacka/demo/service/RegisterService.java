fdd255df7a9debbccb322bb7413183ee28ac4159
Ana Eliza <anaelizaperobelli@gmail.com>
2023-09-24 17:14:21 -0300

    LoginService RegisterService creation

---
+++ src/main/java/com/hacka/demo/service/RegisterService.java
@@ -0,0 +1,23 @@
+package com.hacka.demo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.hacka.demo.entities.Register;
+import com.hacka.demo.repository.RegisterRepository;
+
+public class RegisterService {
+
+	@Autowired
+	private RegisterRepository repository;
+
+	public List<Register> findAll() {
+		return repository.findAll();
+	}
+
+	public Register findById(Integer id) {
+		return repository.findById(id).get();
+	}
+
+}
