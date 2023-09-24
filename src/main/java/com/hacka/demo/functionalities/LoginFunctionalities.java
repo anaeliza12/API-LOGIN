ccf6581fcac7f0c4323ad2915073c7040c9871e7
Ana Eliza <anaelizaperobelli@gmail.com>
2023-09-24 15:36:19 -0300

    LoginFunctionalities, RegisterFunctionalities creation

---
+++ src/main/java/com/hacka/demo/functionalities/LoginFunctionalities.java
@@ -0,0 +1,10 @@
+package com.hacka.demo.functionalities;
+
+import com.hacka.demo.entities.Login;
+
+public class LoginFunctionalities {
+	
+	private Login login;
+	
+
+}
---
+++ src/main/java/com/hacka/demo/functionalities/RegisterFunctionalities.java
@@ -0,0 +1,8 @@
+package com.hacka.demo.functionalities;
+
+import com.hacka.demo.entities.Register;
+
+public class RegisterFunctionalities {
+
+	private Register register;
+}
