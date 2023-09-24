16355a9a2c0736eb5fa24327c96c23b0650966a5
Ana Eliza <anaelizaperobelli@gmail.com>
2023-09-24 15:02:24 -0300

    Register entity creation

--- src/main/java/com/hacka/demo/entities/Register.java
+++ src/main/java/com/hacka/demo/entities/Register.java
@@ -1,5 +1,87 @@
 package com.hacka.demo.entities;
 
+import java.util.Objects;
+
 public class Register {
+	private Integer id;
+	private String name;
+	private String email;
+	private String password;
+	private String confirmPassword;
+
+	public Register() {
+
+	}
+
+	public Register(Integer id, String name, String email, String password, String confirmPassword) {
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.password = password;
+		this.confirmPassword = confirmPassword;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getConfirmPassword() {
+		return confirmPassword;
+	}
+
+	public void setConfirmPassword(String confirmPassword) {
+		this.confirmPassword = confirmPassword;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Register other = (Register) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	@Override
+	public String toString() {
+		return "Register [id=" + id + ", name=" + name + ", email=" + email + ", password=" + password
+				+ ", confirmPassword=" + confirmPassword + "]";
+	}
 
 }
