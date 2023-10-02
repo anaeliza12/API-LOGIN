6836c3b9d9dc3cd8610aa20022aaae0c83652c0b
Ana Eliza <anaelizaperobelli@gmail.com>
2023-10-01 21:52:21 -0300

    API Connection with data inserted by front-end

--- js/script.js
+++ js/script.js
@@ -1,35 +1,51 @@
-const dataEmail = document.getElementById("email")
 
+
+const dataEmail = document.getElementById("email")
+const dataPassword = document.getElementById("password")
 
 const loginData = {
-    email: "ana@gmail",
-    password: "123"
-}
+    email: "",
+    password: ""
 
+}
 const login = async () => {
-    
+
     const options = {
         method: "POST",
         headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(loginData)
-        }
-
-        const url = "http://localhost:8080/login/auth"
-        try {
-
-            const response = await fetch(url, options)
-
-
-        } catch (error) {
-            console.error(error)
-        }
-        
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(loginData)
     }
-    
-    
-    document.addEventListener("DOMContentLoaded", () => {
+    const url = "http://localhost:8080/login/auth"
+    try {
+
+        const connection = await fetch(url, options)
+        const response = connection.status
+
+        if (response == 200) {
+            const log = await connection.text()
+           alert(log)
+
+        }
+
+    } catch (error) {
+        console.log(error)
+    }
+
+
+
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.querySelector("form")
+
+    form.addEventListener('submit', (event) => {
+        event.preventDefault()
+
+        loginData.email = dataEmail.value
+        loginData.password = dataPassword.value
         login()
     })
 
+})
